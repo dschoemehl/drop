0842ae19e6562672d3896480ab8ff3db0d2b5ae6
David Schoemehl <davidschoemehl@gmail.com>
2022-06-02 07:22:05 -0500

    Finished program, updates to .gitignore

--- core/src/com/mygdx/drop/Drop.java
+++ core/src/com/mygdx/drop/Drop.java
@@ -6,13 +6,16 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.TimeUtils;
 
-import sun.jvm.hotspot.oops.VirtualCallTypeData;
+import java.util.Iterator;
 
 
 public class Drop extends ApplicationAdapter {
@@ -24,8 +27,12 @@ public class Drop extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Rectangle bucket;
 	private Vector3 touchPos;
+	private Array<Rectangle> raindrops;
+	private long lastDropTime;
+	static final Rectangle screenSize = new Rectangle(0,0,800,400);
+	static final int moveSpeed = 200;
+	static final Rectangle imageSize = new Rectangle(0,0,64,64);
 
-	
 	@Override
 	public void create () {
 		//Load images for droplet and bucket
@@ -37,50 +44,72 @@ public class Drop extends ApplicationAdapter {
 		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
 
 		//Start playback of rain music
-		//rainMusic.setLooping(true);
-		//rainMusic.play();
+		rainMusic.setLooping(true);
+		rainMusic.play();
 
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 400);
+		camera.setToOrtho(false, screenSize.width, screenSize.height);
 
 		batch = new SpriteBatch();
 
 		bucket = new Rectangle();
-		bucket.x = 800 / 2 - 64 / 2;
+		bucket.x = screenSize.width / 2 - imageSize.width / 2;
 		bucket.y = 20;
-		bucket.width = 64;
-		bucket.height = 64;
+		bucket.width = imageSize.width;
+		bucket.height = imageSize.height;
 
 		touchPos = new Vector3();
+
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0, 0, .2f, 1);
-		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y );
-		batch.end();
 
-		if(Gdx.input.isTouched()){
+		if(Gdx.input.isTouched()) {
 			//Vector3 touchPos = new Vector3();
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touchPos);
-			bucket.x = touchPos.x - 64 / 2;
+			bucket.x = touchPos.x - imageSize.width / 2;
 		}
 
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+			bucket.x -= moveSpeed * Gdx.graphics.getDeltaTime();
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			bucket.x += 200 * Gdx.graphics.getDeltaTime();
+			bucket.x += moveSpeed * Gdx.graphics.getDeltaTime();
 		}
-		if(bucket.x < 0){
+		if(bucket.x < 0) {
 			bucket.x = 0;
 		}
-		if(bucket.x > 800 - 64){
-			bucket.x = 800 - 64;
+		if(bucket.x > screenSize.width - imageSize.width) {
+			bucket.x = screenSize.width - imageSize.width;
 		}
 
+		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) {
+			spawnRaindrop();
+		}
+
+		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext();){
+			Rectangle raindrop = iter.next();
+			raindrop.y -= moveSpeed * Gdx.graphics.getDeltaTime();
+			if(raindrop.y + imageSize.width < 0){
+				iter.remove();
+			}
+			else if (raindrop.overlaps(bucket)){
+				dropSound.play();
+				iter.remove();
+			}
+		}
+
+		ScreenUtils.clear(0, 0, .2f, 1);
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y );
+		for(Rectangle raindrop: raindrops){
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
+		batch.end();
 	}
 	
 	@Override
@@ -90,5 +119,15 @@ public class Drop extends ApplicationAdapter {
 		rainMusic.dispose();
 		bucketImage.dispose();
 		dropImage.dispose();
+	}
+
+	private void spawnRaindrop(){
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, screenSize.width- imageSize.width);
+		raindrop.y = screenSize.height;
+		raindrop.width = imageSize.width;
+		raindrop.height = imageSize.height;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
 	}
 }
